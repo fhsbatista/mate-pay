ca0b1a97126f4d231481c5543acea11688f34357
Fernando Batista <fernandohsbatista@gmail.com>
2025-03-21 11:52:59 -0300

    (wallet_core):(chore: initialized project)

---
+++ wallet_core/src/test/java/com/matepay/wallet_core/WalletCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matepay.wallet_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
