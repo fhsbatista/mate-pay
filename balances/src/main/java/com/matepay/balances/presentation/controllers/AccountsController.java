7f7d46ae66ead5e61a55b78a0603bcc461b4e5cb
Fernando Batista <fernandohsbatista@gmail.com>
2025-04-14 22:56:54 -0300

    (general):(chore: ports and container names changes)

--- balances/src/main/java/com/matepay/balances/presentation/controllers/AccountsController.java
+++ balances/src/main/java/com/matepay/balances/presentation/controllers/AccountsController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("api/accounts")
+@RequestMapping("balances")
 @Validated
 public class AccountsController {
     @Autowired
